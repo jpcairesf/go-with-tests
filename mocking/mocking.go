189356f2be6839eba97d5e0c8332c59881f783c4
jpcairesf <jpcairesf@gmail.com>
2024-10-16 18:42:07 -0300

    feat: Mocking sleep calls

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -4,18 +4,38 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 func main() {
-	Countdown(os.Stdout)
+	Countdown(os.Stdout, &DefaultSleeper{})
 }
 
-func Countdown(out io.Writer) {
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls += 1
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := 3; i > 0; i-- {
 		_, err := fmt.Fprintln(out, i)
 		if err != nil {
 			return
 		}
+		sleeper.Sleep()
 	}
 	_, err := fmt.Fprint(out, "Go!")
 	if err != nil {
